135881a0bf143e0fc45a0c55bf345f84c5585e44
sofia <sofia.leal01@estudiant.upf.edu>
2025-06-01 17:38:43 +0200

    commit document_list.h

--- src/document_list.h
+++ src/document_list.h
@@ -3,34 +3,34 @@
 
 #include "document.h"
 
+//Estructura lista  enlazada con un contador de elementos
 typedef struct DocumentsList {
-    Document* head;
-    int count;
+    Document* head;  //puntero al primer documento de la lista
+    int count;    //num total de documentos en la lista
 } DocumentsList;
 
+//Estructura nodo individual de una lista de IDs de documento
 typedef struct DocumentIdNode {
-    int documentId;
-    struct DocumentIdNode* next;
+    int documentId;    //ID del documento
+    struct DocumentIdNode* next; //sig nodo lista
 } DocumentIdNode;
 
+//Estructura lista completa de IDs de documentos usa una lista enlazada y lleva la cuenta de los elementos.
 typedef struct DocumentIdList {
-    DocumentIdNode* head;
-    int count;
+    DocumentIdNode* head; //puntero al primer nodo
+    int count;   //num de IDs almacenados
 } DocumentIdList;
 
 
-DocumentsList* documentsListCreate();
-void documentsListAppend(DocumentsList* list, Document* doc);
-void documentsListPrint(DocumentsList* list);
-Document* documentsListGet(DocumentsList* list, int index);
-void documentsListFree(DocumentsList* list);
+DocumentsList* documentsListCreate(); //crea una lista de documentos
+void documentsListAppend(DocumentsList* list, Document* doc); //añade un documento a la lista
+void documentsListPrint(DocumentsList* list); //imprime la lista de documentos
+Document* documentsListGet(DocumentsList* list, int index); //obtiene un documento por índice
+void documentsListFree(DocumentsList* list); //libera la memoria de la lista de documentos
 
-DocumentIdList* documentIdListCreate();
-void documentIdListAppend(DocumentIdList* list, int documentId);
-int documentIdListContains(DocumentIdList* list, int documentId);
-void documentIdListFree(DocumentIdList* list);
-
-
-
+DocumentIdList* documentIdListCreate(); // Crea una lista de IDs de documentos
+int documentIdListContains(DocumentIdList* list, int documentId); //verifica si un ID de documento existe en la lista
+void documentIdListAppend(DocumentIdList* list, int documentId); //añade un ID de documento a la lista
+void documentIdListFree(DocumentIdList* list); //libera la memoria de la lista de IDs
 
 #endif //DOCUMENT_LIST_H
